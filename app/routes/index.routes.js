0c4565280faea8c5c87fa2dbc19b98702ffea822
owlsketch <satimidus@gmail.com>
2018-04-27 18:21:38 -0400

    Implemented IPv4 address detection Successfully detected by Mozilla IoT However, can not get any properties now?

--- app/routes/index.routes.js
+++ app/routes/index.routes.js
@@ -25,26 +25,4 @@ module.exports = function(app, device) {
 
   app.route('/properties/brightness')
     .put(addDevice, controller.brightness);
-
-
-/*
-  //root description json file
-  app.route('/things/pixelwall')
-    .get(controller.description);
-
-  //returns JSON description of immediate attributes of thing
-  app.route('/things/pixelwall/:attribute')
-    .get(controller.attribute);
-
-  //pass board object as part of req in our own middleware
-  //return values of property
-  app.route('/things/pixelwall/properties/:property')
-    .get(addDevice, controller.property);
-
-  app.route('/things/pixelwall/properties/on')
-    .put(addDevice, controller.on);
-
-  app.route('/things/pixelwall/properties/brightness')
-    .put(addDevice, controller.brightness);
-*/
 };
--- index.js
+++ index.js
@@ -2,25 +2,23 @@ let device = require('./config/device.js')();
 let app = require('./config/express.js')(device);
 let server = require('http').Server(app);
 let dnssd = require('dnssd');
+let localIPv4Address = require('local-ipv4-address');
 
 const PORT = process.env.PORT || 3000;
 
-let dns = new dnssd.Advertisement(
-  new dnssd.ServiceType('_http._tcp,_webthing'),
-  PORT, 
-  { 
-    name: 'pixel wall', 
-    txt: {
-      url: 'DESKTOP-1CJCEB2.local'
-    }
-  });
-  
+localIPv4Address().then((ipAddress) => {
+  let url = 'http://' + ipAddress + ':' + PORT + '/';
 
-device.board.on('ready', function() {
-  device.setup();
-  server.listen(PORT, function() {
-    console.log('Server listening on port ' + PORT );
-    dns.start();
-    console.log(dns);
+  let dns = new dnssd.Advertisement(
+    new dnssd.ServiceType('_http._tcp,_webthing'),
+    PORT, { name: 'pixel wall', txt: { url } }
+  );
+
+  device.board.on('ready', function() {
+    device.setup();
+    server.listen(PORT, function() {
+      console.log('Server listening on port ' + PORT );
+      dns.start();
+    });
   });
 });
