dd3c02a6732b267762b63cebae89c7fa8e033736
rianders <rianders@docs.rutgers.edu>
2019-05-16 23:37:34 -0700

    Ugly hack. Old code added to replace missing code on raspberry pi with no code clean up. Yuck

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ let server = require('http').Server(app);
 let dnssd = require('dnssd');
 let localIPv4Address = require('local-ipv4-address');
 
-const PORT = process.env.PORT || 3000;
-
 localIPv4Address().then((ipAddress) => {
   let url = 'http://' + ipAddress + ':' + PORT + '/';
 
@@ -14,12 +12,62 @@ localIPv4Address().then((ipAddress) => {
     new dnssd.ServiceType('_http._tcp,_webthing'),
     PORT, { name: 'pixel wall', txt: { url } }
   );
+	
+let boardReady = false;
+const PORT = process.env.PORT || 3000;
+
+let socket = require('socket.io-client')('http://localhost:3001');
+
+let cols = device.properties.width;
+let rows = device.properties.height;;
+
+let initialDesign = [];
+for(let i = 0; i < cols*rows; i++) {
+	  initialDesign.push([255,255,255]);
+}
+
+
+	socket.on('connect', function() {
+		  socket.emit('identifier', {
+			      type: 'device',
+			      dimmensions: {
+				            width: cols,
+				            height: rows
+				          },
+			      boardState: initialDesign
+			    });
+	});
+
+	socket.on('read', function(data) {
+		if(boardReady) {
+			switch(data.type) {
+				case 'single-pixel':
+					let newPixelNum = device.leds.sortPixel(data.pixel, cols, rows);
+					device.leds.setPixelColor(newPixelNum, data.color[0], data.color[1], data.color[2]);
+					device.leds.show();
+					break;
+				case 'all-pixels':
+					device.leds.pushImage(data.colors, cols, rows);
+					device.leds.setImage(ledstrip, cols, rows);
+					device.leds.show();
+					break;
+				default:
+					break;
+			}
+		}
+	});
+
 
   device.board.on('ready', function() {
     device.setup();
+    boardReady = true;
+
+   
     server.listen(PORT, function() {
       console.log('Server listening on port ' + PORT );
       dns.start();
+
+    
     });
   });
 });
